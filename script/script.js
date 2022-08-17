8f552d39410e54b838e08e493407155218a301da
codersbenchworks <codersbenchworks@gmail.com>
2022-08-17 09:24:29 +0530

    indo gargaar

---
+++ script/script.js
@@ -0,0 +1,8 @@
+$('.banner-slider-section .banner-slider').slick({
+    infinite: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow:"<button type='button' class='slick-prev pull-left'></button>",
+    nextArrow:"<button type='button' class='slick-next pull-right'></button>"
+    });
